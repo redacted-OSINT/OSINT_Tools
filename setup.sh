25aee204133a7b68996d7d07776cf5cfaee83cd4
redacted-OSINT <143467216+redacted-OSINT@users.noreply.github.com>
2023-08-28 21:44:12 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install pip -y
+sudo apt install nmap -y
+sudo pip install BeautifulSoup
+
+sudo apt update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y

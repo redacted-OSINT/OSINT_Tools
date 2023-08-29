1d1fad6a8974a34f77e7daf82056c2e9615c36dc
redacted-OSINT <143467216+redacted-OSINT@users.noreply.github.com>
2023-08-29 15:23:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt update
-
-sudo apt install pip -y
-sudo apt install nmap -y
-sudo pip install BeautifulSoup
-
-sudo apt update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
+
+pip install beautifulsoup4
+sudo apt install nmap -y
+pip install shodan
+
+
+

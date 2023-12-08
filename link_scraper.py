bd74ede812aabfa30302359684ec9db142332ca0
redacted-OSINT <143467216+redacted-OSINT@users.noreply.github.com>
2023-12-08 09:19:44 -0800

    Update link_scraper.py

--- link_scraper.py
+++ link_scraper.py
@@ -7,18 +7,22 @@ print('''
  |________||_____||_____|\____||____||____| |______|   
                                                        
                                                        ''')
-print('-' *80)
-target = input('Enter target website including http or https: ')
-print('-' *80)
 
 import requests
 from bs4 import BeautifulSoup
 
-text = requests.get(target).text
-soup = BeautifulSoup(text, 'html.parser')
-tags = soup.find_all('a')
-with open('link.txt', 'w') as f:
-    for link in tags:
-        results = link.get('href')
-        if 'http' in results:
-            f.write(results + '\n')
+def grab_links(url):
+    text = requests.get(url).text
+    soup = BeautifulSoup(text, 'html.parser')
+    tags = soup.find_all('a')
+    with open('results.txt', 'a') as f:
+        for tag in tags:
+            link = tag.get('href')
+            f.write(link + '\n')
+
+def main():
+    url = input('Enter target URL here: ')
+    grab_links(url)
+
+if __name__ == "__main__":
+    main()
